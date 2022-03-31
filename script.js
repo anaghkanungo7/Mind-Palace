22218baf09f7aaf1d31642e96bb6a63b7571bbca
Glitch (glitch-hello-website) <none>
2022-03-31 00:08:21 +0000

    🌷💋 Checkpoint ./index.html:43072139/1395 ./script.js:43072139/279 ./style.css:43072139/1185

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ This is your site JavaScript code - you can add interactivity and carry out proc
 // Print a message in the browser's dev tools console each time the page loads
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
+ocument.getElementById('GFG').innerHTML
+                = 'A computer science portal for geeks';
 
 /* 
 Make the "Click me!" button move when the visitor clicks it:
